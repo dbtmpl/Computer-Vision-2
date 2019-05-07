5474a103e466a02c2d0129815502fe2c6a386819
Maurice Frank <maurice.frank@posteo.de>
2019-05-08 01:42:49 +0200

    SfM

--- Assignment_2/structure_from_motion.py
+++ Assignment_2/structure_from_motion.py
@@ -1,6 +1,4 @@
 import numpy as np
-import cv2 as cv
-from scipy.linalg import orthogonal_procrustes
 from procrustes import procrustes
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
@@ -14,31 +12,18 @@ def structure_from_motion(point_view_matrix, block_size):
     :param block_size: 
     :return: 
     """
-    print("SFM starting")
-    print("............")
-    m, n = int(point_view_matrix.shape[0]), point_view_matrix.shape[2]
-    # m, n = int(point_view_matrix.shape[0] / 2), point_view_matrix.shape[1]
+    m, n = int(point_view_matrix.shape[0] / 2), point_view_matrix.shape[1]
+    print("SFM starting\n.........\n#Images: {}, #Points: {}".format(m, n))
 
-    world_coordinates = np.zeros((n, 3))
-    # point_view_matrix = point_view_matrix.reshape(m, 2, n)
+    model = np.zeros((n, 3))
+    for i in range(m - (block_size - 1)):
+        pvm_rows = point_view_matrix[i:i+2*block_size, ...]
+        dense_idx = np.all(pvm_rows, axis=0)
+        world_idx = np.all(model, axis=1)
 
-    print("M, N")
-    print(m, n)
-
-    print(point_view_matrix.shape[0])
-    print(block_size - 1)
-
-    for i in np.arange(m - (block_size - 1)):
-        sparse_block = point_view_matrix[i:block_size + i, :, :].reshape(2 * block_size, n)
-        dense_indices = np.all(sparse_block, axis=0)
-        filled_wc_indices = np.all(world_coordinates, axis=1)
-        # new_indices = np.where(np.invert(filled_wc_indices[dense_indices])[0])
-        new_indices = np.invert(filled_wc_indices[dense_indices])
-
-        if np.any(new_indices):
-
-            D = sparse_block[:, dense_indices]
-            D = D - np.mean(D, axis=1).reshape(2 * block_size, 1)
+        if np.any(~world_idx[dense_idx]):
+            D = pvm_rows[:, dense_idx]
+            D = D - D.mean(axis=1)[:, None]
 
             U, W, Vt = np.linalg.svd(D)
             V = Vt.T
@@ -50,43 +35,33 @@ def structure_from_motion(point_view_matrix, block_size):
             S[0, :] = -S[0, :]
             S[1, :] = -S[1, :]
 
-            if not np.any(filled_wc_indices):
-                world_coordinates[dense_indices, :] = S.T
+            if not np.any(world_idx):
+                model[dense_idx, :] = S.T
 
             else:
-                X = world_coordinates[np.logical_and(filled_wc_indices, dense_indices), :]
-                Y = S[:, np.invert(new_indices)].T
+                X = model[world_idx & dense_idx, :]
+                Y = S[:, world_idx[dense_idx]].T
+                _, _, (R, s, t) = procrustes(X, Y)
+                Z = s * S.T @ R + t
+                model[dense_idx, :] = Z
 
-                d, Z, transformation = procrustes(X, Y)
-                R, s, t = transformation
-
-                _S = s * S.T @ R + t
-
-                _transformed_points = _S[new_indices, :]
-                world_coordinates[np.logical_and(dense_indices, np.invert(filled_wc_indices)), :] = _transformed_points
-
-    world_coordinates = world_coordinates[filled_wc_indices, :]
-    # world_coordinates = np.delete(world_coordinates, 200, axis=0)
+    model = model[world_idx, :]
+    model = model[model[:, 2] > -1, :]  # Hacky filter, sry
 
     # # Plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-
-    x = world_coordinates[:, 0]
-    y = world_coordinates[:, 1]
-    z = world_coordinates[:, 2]
-
+    x, y, z = model[:, 0], model[:, 1],model[:, 2]
     ax.scatter(x, y, z, c='r', marker='o')
     ax.view_init(20, 00)
-
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
-    # ax.set_zlim3d(-2,0)
-
     plt.show()
 
     cloud = o3d.PointCloud()
-    cloud.points = o3d.Vector3dVector(world_coordinates)
-    o3d.draw_geometries([cloud], left=0, top=0)
+    cloud.points = o3d.Vector3dVector(model)
+    o3d.draw_geometries([cloud])
+
+
+if __name__ == '__main__':
+    pvm = np.loadtxt('PointViewMatrix.txt')
+    structure_from_motion(pvm, 3)
+
